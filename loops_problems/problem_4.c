d4581341dd6e13e2272cb07c2934c83c138cf05c
Intisar Muhib <muhibvai007@gmail.com>
2026-06-23 21:02:16 +0600

    Implement code changes to enhance functionality and improve performance

--- loops_problems/problem_4.c
+++ loops_problems/problem_4.c
@@ -1,6 +1,17 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main ()
+int main()
 {
-  
+    int a, b, sum, n, i;
+    scanf("%d", &n);
+    
+    i = 1;
+    while (i <= n)
+    {
+       scanf("%d %d", &a, &b);
+       sum = a+b;
+       printf("%d\n",sum);
+       i++;
+    }
+    
 }
