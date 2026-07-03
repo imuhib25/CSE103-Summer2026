9faa6dfd21880798f69b1d795ea45a45cf270ef9
Intisar Muhib <muhibvai007@gmail.com>
2026-07-04 01:16:41 +0600

    Implement voting system in problem_8.c and add .gitignore for compiled executables

--- array_problems/problem_8.c
+++ array_problems/problem_8.c
@@ -5,5 +5,28 @@ int main ()
     int m,n;
     printf("Number of candidates: ");
     scanf("%d", &m);
+    printf("Number of votes: ");
+    scanf("%d", &n);
+    printf("Votes: ");
+    int votes[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &votes[i]);
+    }
+    int count[m];
+    for (int i = 0; i < m; i++)
+    {
+        count[i]=0;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if(votes[i] >= 1 && votes[i] <= m)
+        {
+            count[votes[i]-1]++;
+        }
+    }
     
+    for (int i = 0; i < m; i++) {
+        printf("Candidate %d received %d votes\n", i + 1, count[i]);
+    }
 }
