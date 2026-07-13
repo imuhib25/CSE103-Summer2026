4a3bd3b6f55a63d22fb53b698046f489e18f50c7
Intisar Muhib <muhibvai007@gmail.com>
2026-07-13 18:18:21 +0600

    Refactor code structure for improved readability and maintainability

--- assignment_1/question_5.c
+++ assignment_1/question_5.c
@@ -1,6 +1,37 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main ()
+int main()
 {
-    int 
+    int N;
+    int n1 = 0, n2 = 0, n3 = 0, n4 = 0, n5 = 0, n6 = 0;
+    printf("Enter number of rolls (N): ");
+    scanf("%d", &N);
+
+    int arr[N];
+    printf("Enter roll results: ");
+    for (int i = 0; i < N; i++)
+    {
+        scanf("%d", &arr[i]);
+        if (arr[i] == 1)
+            n1++;
+        if (arr[i] == 2)
+            n2++;
+        if (arr[i] == 3)
+            n3++;
+        if (arr[i] == 4)
+            n4++;
+        if (arr[i] == 5)
+            n5++;
+        if (arr[i] == 6)
+            n6++;
+    }
+
+    printf("Number 1: %d\n", n1);
+    printf("Number 2: %d\n", n2);
+    printf("Number 3: %d\n", n3);
+    printf("Number 4: %d\n", n4);
+    printf("Number 5: %d\n", n5);
+    printf("Number 6: %d\n", n6);
+
+    return 0;
 }
